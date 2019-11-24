aec5acae988ffae78b5db41e09b263918e534cff
nandoogouveia <gouveiaramos.me@gmail.com>
2019-11-24 02:10:17 +0000

    dia 20

---
+++ src/Handler/Home.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies #-}
+module Handler.Home where
+
+import Import
+--import Network.HTTP.Types.Status
+import Database.Persist.Postgresql()
+
+getHomeR :: Handler Html
+getHomeR = undefined
