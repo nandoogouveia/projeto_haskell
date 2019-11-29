d401f01a38e24e3ce8d7348acec1ed32d5c210a2
nandoogouveia <gouveiaramos.me@gmail.com>
2019-11-29 00:42:33 +0000

    atualizando index 1

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -55,4 +55,9 @@ getHomeR = do
         <div class="body-wrap">
             <header class="site-header">
                 <div class="container">
+                    <div class="site-header-inner">
+                        <div class="brand header-brand">
+                            <h1 class="m-0">
+    							<a href="#">
+    								<img class="header-logo-image" src="@StaticR img_logo_svg" alt="Logo">
         |]    
