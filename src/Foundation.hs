16b38a83bca4ebe7dd82cc98528e92ce92a7a2cc
nandoogouveia <gouveiaramos.me@gmail.com>
2019-11-29 23:23:32 +0000

    eh usuario eh admin found

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -24,6 +24,25 @@ mkYesodData "App" $(parseRoutesFile "config/routes")
 instance Yesod App where
     makeLogger = return . appLogger
 
+isAdmin :: Handler AuthResult
+isAdmin = do 
+    sess <- lookupSession "_NOME"
+    case sess of 
+        Nothing -> return AuthenticationRequired
+        Just "admin" -> return Authorized
+        Just _ -> return $ Unauthorized "VC EH USUARIO COMUM"
+
+isUsuario :: Handler AuthResult
+isUsuario = do 
+    sess <- lookupSession "_NOME"
+    case sess of 
+        Nothing -> return AuthenticationRequired
+        Just _ -> return Authorized
+
+type Form a = Html -> MForm Handler (FormResult a, Widget)
+
+
+
 instance YesodPersist App where
     type YesodPersistBackend App = SqlBackend
     runDB action = do
