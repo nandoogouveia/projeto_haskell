886ae28c615a74331866cadf1c9121a54fdca392
nandoogouveia <gouveiaramos.me@gmail.com>
2019-11-29 22:59:08 +0000

    routes usuario/admin

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -31,29 +31,29 @@ getHomeR = do
                 
             [whamlet|
         
-                -- <div id="48248-6">
-                --     <script src="//ads.themoneytizer.com/s/gen.js?type=6">
-                --     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=6">
+                 <div id="48248-6">
+                     <script src="//ads.themoneytizer.com/s/gen.js?type=6">
+                     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=6">
                 
                 
                 
-                -- <div style="text-align:center;" id="48248-31">
-                --     <script src="//ads.themoneytizer.com/s/gen.js?type=31">
-                --     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=31">
+                 <div style="text-align:center;" id="48248-31">
+                     <script src="//ads.themoneytizer.com/s/gen.js?type=31">
+                     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=31">
                 
                 
-                -- <div id="48248-2">
-                --     <script src="//ads.themoneytizer.com/s/gen.js?type=2">
-                --     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=2">
+                 <div id="48248-2">
+                     <script src="//ads.themoneytizer.com/s/gen.js?type=2">
+                     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=2">
                 
                 
                 
-                <div class="body-wrap">
-                    <header class="site-header">
-                        <div class="container">
-                            <div class="site-header-inner">
-                                <div class="brand header-brand">
-                                    <h1 class="m-0">
-            							<a href="#">
-            								<img src="static/img/logo.svg" class="header-logo-image" alt="Logo">
+                --<div class="body-wrap">
+                  --  <header class="site-header">
+                    --    <div class="container">
+                      --      <div class="site-header-inner">
+                        --        <div class="brand header-brand">
+                          --          <h1 class="m-0">
+                            --            <a href="#">
+                              --              <img src="static/img/logo.svg" class="header-logo-image" alt="Logo">
         |]    
