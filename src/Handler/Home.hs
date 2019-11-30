7d5ed701e72a34fcc92f7b01d8974e28e30c5901
nandoogouveia <gouveiaramos.me@gmail.com>
2019-11-30 01:33:03 +0000

    correcao homehs

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -20,40 +20,38 @@ getAdsR = return $ TypedContent "text/plain"
 
 getHomeR :: Handler Html
 getHomeR = do 
-    -- sess <- lookupSession "_NOME"
+    sess <- lookupSession "_NOME"
     defaultLayout $ do 
         -- addScriptRemote "url" -> CHAMA JS EXTERNO
         -- addScript (StaticR script_js), ONDE script 
         -- eh o nome do seu script.
         -- pasta css, arquivo: bootstrap.css
-        setTitle "Teste"
         addStylesheet (StaticR css_bootstrap_css)
-                
-            [whamlet|
         
-                 <div id="48248-6">
-                     <script src="//ads.themoneytizer.com/s/gen.js?type=6">
-                     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=6">
-                
-                
-                
-                 <div style="text-align:center;" id="48248-31">
-                     <script src="//ads.themoneytizer.com/s/gen.js?type=31">
-                     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=31">
-                
-                
-                 <div id="48248-2">
-                     <script src="//ads.themoneytizer.com/s/gen.js?type=2">
-                     <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=2">
-                
-                
-                
-                --<div class="body-wrap">
-                  --  <header class="site-header">
-                    --    <div class="container">
-                      --      <div class="site-header-inner">
-                        --        <div class="brand header-brand">
-                          --          <h1 class="m-0">
-                            --            <a href="#">
-                              --              <img src="static/img/logo.svg" class="header-logo-image" alt="Logo">
+        toWidgetHead [julius|
+            function ola(){
+                alert("ola");
+            }
+        |]
+        [whamlet|
+        
+        <div id="48248-6">
+        <script src="//ads.themoneytizer.com/s/gen.js?type=6">
+        <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=6">
+        
+        
+        
+        <div style="text-align:center;" id="48248-31">
+        <script src="//ads.themoneytizer.com/s/gen.js?type=31">
+        <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=31">
+        
+        
+        <div id="48248-2">
+        <script src="//ads.themoneytizer.com/s/gen.js?type=2">
+        <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=2">
+        
+        
+        
+            <h1>
+                IAEw!
         |]    
