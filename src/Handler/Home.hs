6b465b12719e6eda8f3652cc1bf3310c3e0397da
nandoogouveia <gouveiaramos.me@gmail.com>
2019-12-03 23:35:22 +0000

    arrumando index

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -34,35 +34,35 @@ getHomeR = do
             }
         |]
         [whamlet|
-        
-                     
-        <div id="48248-6">
-        <script src="//ads.themoneytizer.com/s/gen.js?type=6">
-        <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=6">
-        <body>
-        <ul>
-           <li>
-              <a href=@{AdminR}>
+            <div id="48248-6">
+              <script src="//ads.themoneytizer.com/s/gen.js?type=6">
+              <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=6">
+            <ul>
+                <li>
+                    <a href=@{AdminR}>
                         ADMINISTRADOR
-           <li>
-              <a href=@{EntrarR}>
+                <li>
+                    <a href=@{EntrarR}>
                         LOGIN DE USUARIO
-            <li>
-              <a href=@{SairR}>
+                <li>
+                    <a href=@{SairR}>
                         SAIR DA SESSAO
-         
-    
+                        
+            <div style="text-align:center;" id="48248-31">
+                <script src="//ads.themoneytizer.com/s/gen.js?type=31">
+                <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=31">
         
+            <div class="body-wrap">
+                <header class="site-header">
+                    <div class="container">
+                        <div class="site-header-inner">
+                            <div class="brand header-brand">
+                                <h1 class="m-0">
+                                    <a href="#">
+                                        <img class="header-logo-image" src="dist/images/logo.svg" alt="Logo">
         
-        <div style="text-align:center;" id="48248-31">
-           <script src="//ads.themoneytizer.com/s/gen.js?type=31">
-           <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=31">
-        
-        <footer>
-            <div id="48248-2">
-               <script src="//ads.themoneytizer.com/s/gen.js?type=2">
-               <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=2">
-        
-        
-        
+            <footer>
+                <div id="48248-2">
+                    <script src="//ads.themoneytizer.com/s/gen.js?type=2">
+                    <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=2">
         |]    
