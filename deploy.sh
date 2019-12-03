3e8994ae13037b24db59a78a46cef571deb05e80
nandoogouveia <gouveiaramos.me@gmail.com>
2019-12-03 14:13:51 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ ssh root@40.71.195.246 <<EOF
 cd projeto_haskell &&
 git pull origin master &&
 stack build &&
-lsof -i:80 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
-nohup stack exec aulahaskell > /dev/null 
+lsof -i:443 -Fp | sed 's/^p//' | head -n -1 | xargs kill -9;
+nohup sudo stack --allow-different-user exec aulahaskell > /dev/null 
 echo "deploy finished"
 EOF
