d406f84e568ec10379c218d9bc3aa319569dab44
nandoogouveia <gouveiaramos.me@gmail.com>
2019-12-03 03:02:42 +0000

    li TEMA PALESTRA COACHES

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -46,7 +46,8 @@ getHomeR = do
                         PALESTRA
             <li>
               <a href=@{ElencoR}>
-              
+                        COACHES
+                        
         <div id="48248-6">
         <script src="//ads.themoneytizer.com/s/gen.js?type=6">
         <script src="//ads.themoneytizer.com/s/requestform.js?siteId=48248&formatId=6">
