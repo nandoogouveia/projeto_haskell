917415234592c5b74ce9fc66e51632bd06af411e
nandoogouveia <gouveiaramos.me@gmail.com>
2019-12-04 00:14:08 +0000

    arrumando index

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -59,7 +59,7 @@ getHomeR = do
                             <div class="brand header-brand">
                                 <h1 class="m-0">
                                     <a href="#">
-                                        <img class="header-logo-image" src=@{StaticR logo_svg} alt="Logo">
+                                        <img class="header-logo-image" src=@{StaticR img_logo_svg} alt="Logo">
         
             <footer>
                 <div id="48248-2">
